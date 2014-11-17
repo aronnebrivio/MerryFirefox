40927f27ed3a99a15904610023d776f408b91085
Aronne Brivio <aroblu94@gmail.com>
2014-11-17 16:58:50 +0100

    Second commit

--- script/script.js
+++ script/script.js
@@ -1,9 +1,15 @@
-window.onload = function() {
-	var s = "<marquee direction='down' scrollamount='"
-	var s1 = "' style='top: 0px; left: "
-	var s2 = "%; height: "
-	var s3 = "%;'>*</marquee>"
-	for(var i = 0; i < 100; i++) {
-		$('body').append(s + parseInt(Math.random() * 100)%13 + s1 + parseInt(Math.random() * 100) + s2 + parseInt(Math.random() * 100) + s3);
-	}
-}
+$(document).ready(function() {
+	console.log("Loaded.");
+	window.addEventListener("devicemotion", function(e){
+		console.log(e.rotationRate)
+	}, true);
+	//window.ondevicemotion = function() {
+		var s = "<marquee direction='down' scrollamount='"
+		var s1 = "' style='top: 0px; left: "
+		var s2 = "%; height: "
+		var s3 = "%;'>*</marquee>"
+		for(var i = 0; i < 50; i++) {
+			$('body').append(s + parseInt(Math.random() * 100)%13 + s1 + parseInt(Math.random() * 100) + s2 + parseInt(Math.random() * 100) + s3);
+		}
+	//}
+});
