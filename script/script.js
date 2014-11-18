67ca0a5e5bb519a98794d556ef7ffdfe5cf6c181
Aronne Brivio <aroblu94@gmail.com>
2014-11-18 16:48:21 +0100

    Handling rotation on z and x axes could work (need some tests with my phone)

--- script/script.js
+++ script/script.js
@@ -17,18 +17,42 @@ window.onload = function() {
 		game.stage.backgroundColor = '#3B5998';
 		//  We're going to be using physics, so enable the Arcade Physics system
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		game.physics.arcade.gravity.y = 300;
+		game.physics.arcade.gravity.y = 0;
+		game.physics.arcade.gravity.x = 0;
 
-		/*window.addEventListener("deviceorientation", function(e){
-			console.log(e.beta);
-			console.log("up = " + up);
-			console.log("down = " + down);
-			// Handling device rotation
-			if(e.beta > 80 && e.beta < 110)
+		// Handling device rotation
+		window.addEventListener("deviceorientation", function(e){
+			// Handling device up-down rotation
+			if(e.beta > 80 && e.beta <= 110)
+				game.physics.arcade.gravity.y = 300;
+			else if(e.beta > 55 && e.beta <= 80)
+				game.physics.arcade.gravity.y = 200;
+			else if(e.beta > 20 && e.beta <= 55)
 				game.physics.arcade.gravity.y = 100;
-			if(e.beta < -80 && e.beta > -110)
+			else if(e.beta < -20 && e.beta >= -55)
 				game.physics.arcade.gravity.y = -100;
-		}, true);*/
+			else if(e.beta < -55 && e.beta >= -80)
+				game.physics.arcade.gravity.y = -200;
+			else if(e.beta < -80 && e.beta > -110)
+				game.physics.arcade.gravity.y = -300;
+			else
+				game.physics.arcade.gravity.y = 0;
+			// Handling device left-right rotation
+			if(e.alpha > 80 && e.alpha <= 110)
+				game.physics.arcade.gravity.x = 300;
+			else if(e.alpha > 55 && e.alpha <= 80)
+				game.physics.arcade.gravity.x = 200;
+			else if(e.alpha > 20 && e.alpha <= 55)
+				game.physics.arcade.gravity.x = 100;
+			else if(e.alpha < -20 && e.alpha >= -55)
+				game.physics.arcade.gravity.x = -100;
+			else if(e.alpha < -55 && e.alpha >= -80)
+				game.physics.arcade.gravity.x = -200;
+			else if(e.alpha < -80 && e.alpha > -110)
+				game.physics.arcade.gravity.x = -300;
+			else
+				game.physics.arcade.gravity.x = 0;
+		}, true);
 		// Adding sprite
 		addFlake(0);
 	}
@@ -46,17 +70,29 @@ window.onload = function() {
 		flake.body.gravity.y = -(Math.random()*100)%80;
 
 		if (deepness < 100) {
-			setTimeout(function() {
+			//setTimeout(function() {
 				deepness = deepness + 1;
 				addFlake(deepness);
-			}, 10);
+			//}, 10);
 		}
 	}
 
-	$(document).on('click', '#up', function () {
+	/*$(document).on('click', '#up', function () {
 		game.physics.arcade.gravity.y = -300;
 	});
 	$(document).on('click', '#down', function () {
 		game.physics.arcade.gravity.y = 300;
 	});
+	$(document).on('click', '#right', function () {
+		game.physics.arcade.gravity.x = 300;
+	});
+	$(document).on('click', '#left', function () {
+		game.physics.arcade.gravity.x = -300;
+	});
+	$(document).on('click', '#stop_up_down', function () {
+		game.physics.arcade.gravity.y = 0;
+	});
+	$(document).on('click', '#stop_left_right', function () {
+		game.physics.arcade.gravity.x = 0;
+	});*/
 }
