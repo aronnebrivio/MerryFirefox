dfc47c654253ec502dd920fe735df472fedf339b
Aronne Brivio <aroblu94@gmail.com>
2014-11-18 15:12:29 +0100

    ...

--- script/script.js
+++ script/script.js
@@ -1,6 +1,4 @@
-console.log("asd");
 window.onload = function() {
-	console.log("Loaded!");
 	var w = window.innerWidth;
 	var h = window.innerHeight;
 	var game = new Phaser.Game(w, h, Phaser.AUTO, '', {
@@ -22,14 +20,23 @@ window.onload = function() {
 		game.physics.arcade.gravity.y = 100;
 
 		// Adding sprite
-		flake = game.add.sprite(100, 96, 'flake');
-		// Adding physic to sprite1
-		game.physics.enable( [ flake ], Phaser.Physics.ARCADE);
-
-	 	flake.body.collideWorldBounds = true;
-		// Coefficente di rimbalzo
-		flake.body.bounce.y = 0;
+		addFlake(0);
 	}
 	function render() {
+	}
+
+	function addFlake(deepness) {
+		flake = game.add.sprite((Math.random()*10000)%w, 0, 'flake');
+		// Adding physic to sprite1
+		game.physics.enable( [ flake ], Phaser.Physics.ARCADE);
+		flake.body.collideWorldBounds = true;
+		// Coefficente di rimbalzo
+		flake.body.bounce.y = 0;
+		if (deepness < 100) {
+			setTimeout(function() {
+				deepness = deepness + 1;
+				addFlake(deepness);
+			}, 10);
+		}
 	}
 }
